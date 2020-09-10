1398627df3f8c9b6bfc497d0168ed2dfb2c4cf2f
wangxu <ttian226@gmail.com>
2020-09-10 11:44:38 +0800

    初始化redux-todolist

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import TodoApp from "./TodoApp";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <TodoApp />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
